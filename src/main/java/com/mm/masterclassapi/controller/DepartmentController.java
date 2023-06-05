b0be960b84961a7f065a8bd9b128080f15dc507b
a233057 <meluleki.madlala@standardbank.co.za>
2023-06-05 19:08:25 +0200

    fetch departments + fetch a single department implementation

--- src/main/java/com/mm/masterclassapi/controller/DepartmentController.java
+++ src/main/java/com/mm/masterclassapi/controller/DepartmentController.java
@@ -3,11 +3,11 @@ package com.mm.masterclassapi.controller;
 import com.mm.masterclassapi.model.Department;
 import com.mm.masterclassapi.service.DepartmentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-@RestController
+import java.util.List;
+
+@RestController("/api/v1")
 public class DepartmentController {
 
     @Autowired
@@ -17,4 +17,15 @@ public class DepartmentController {
     public Department saveDepartment(@RequestBody Department department) {
         return departmentService.saveDepartment(department);
     }
+
+    @GetMapping("/departments")
+    public List<Department> fetchDepartmentList() {
+        return departmentService.fetchDepartmentList();
+    }
+
+    @GetMapping("/department/{id}")
+    public Department fetchDepartmentById(@PathVariable("id") Long departmentId) {
+        return departmentService.fetchDepartmentById(departmentId);
+    }
+
 }
--- src/main/java/com/mm/masterclassapi/service/DepartmentService.java
+++ src/main/java/com/mm/masterclassapi/service/DepartmentService.java
@@ -2,6 +2,10 @@ package com.mm.masterclassapi.service;
 
 import com.mm.masterclassapi.model.Department;
 
+import java.util.List;
+
 public interface DepartmentService {
     Department saveDepartment(Department department);
+    List<Department> fetchDepartmentList();
+    Department fetchDepartmentById(Long departmentId);
 }
