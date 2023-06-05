51167e214bcf3f650db519f52dddc316a92e832a
a233057 <meluleki.madlala@standardbank.co.za>
2023-06-05 15:12:27 +0200

    first commit

---
+++ src/main/java/com/mm/masterclassapi/MasterclassApiApplication.java
@@ -0,0 +1,13 @@
+package com.mm.masterclassapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasterclassApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasterclassApiApplication.class, args);
+	}
+
+}
